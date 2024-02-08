b7067b7247b8ec82764134653fc2439c7fe923b8
Abosi-Godwin <bbnl6060@gmail.com>
2024-02-08 18:43:19 +0100

    update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -14,20 +14,48 @@ Note: For 4 or more names, the number in "and 2 others" simply increases.
 */
 
 const getActualLikes = names => {
+
   if (names.length === 1) {
-    return `${[...names]} likes this posts`;
-  } else if(names.length === 2) {
-    return `${names[0]} and ${names[1]} likes this posts`;
+    return `${[...names]} likes this`;
+  } else if (names.length === 2) {
+
+    return `${names[0]} and ${names[1]} like this`;
+
   } else if (names.length === 3) {
-     return `${names[0]}, ${names[1]} and ${names[2]} likes this posts`;
-   } else {
-     return `${names[0]} and ${names.length - 1} others likes this post`;
-   }
+
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+
+  } else {
+
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+
+  }
+
 }
 
-const countLikes = names => {
- const res = names.length === 0 ? "no one likes this" : getActualLikes(names);
- return res;
+const likes = names => {
+
+  const res = names.length === 0 ? "no one likes this" : getActualLikes(names);
+
+  return res;
+
 }
 
-console.log(countLikes(["Margret","Alex", "Jacob", "Mark", "Max", "Godwin"]))
+//console.log(likes(["Margret", "Alex", "Jacob", "Mark", "Max", "Godwin"]));
+
+
+//Exercise 2 => Sentence Smash
+/* 
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+*/
+// Example
+/*
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+*/
+
+// solution.......
+
+const smash = word => word.join(" ").trim();
+
+//console.log(smash(['hello', 'world', 'this', 'is', 'great']))
+
